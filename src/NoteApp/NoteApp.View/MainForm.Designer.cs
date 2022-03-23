ec507919309bb3c4fe4050d5a8d5c59b19a6a080
La Dispute <cckkcckk666888@gmail.com>
2022-03-23 16:40:50 +0700

    commit

--- src/NoteApp/NoteApp.View/MainForm.Designer.cs
+++ src/NoteApp/NoteApp.View/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NoteApp.View
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -31,7 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "MainForm";
         }
 
         #endregion
